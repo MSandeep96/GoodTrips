8740d949e0c4f6a0ebe2451a17dc0efeee6abcae
Varun Kathuria <chocoshake2007@gmailcom>
2016-04-23 21:59:33 +0530

    Icons Added

---
+++ app/src/main/java/com/droidboys/goodtrips/Interfaces/ApiKeys.java
@@ -0,0 +1,13 @@
+package com.droidboys.goodtrips.Interfaces;
+
+/**
+ * Created by VARUN on 4/23/2016.
+ */
+public interface ApiKeys {
+    long id;
+    long place;
+    String description;
+    long given_by;
+    boolean is_positive;
+
+}
