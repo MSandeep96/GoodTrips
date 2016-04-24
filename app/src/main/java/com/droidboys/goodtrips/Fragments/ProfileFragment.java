e214eb65eab963d4f0d0a1eb3c4cd86fe227eabb
Sande <msande1996@gmail.com>
2016-04-24 12:28:40 +0530

    Added items

--- app/src/main/java/com/droidboys/goodtrips/Fragments/ProfileFragment.java
+++ app/src/main/java/com/droidboys/goodtrips/Fragments/ProfileFragment.java
@@ -6,14 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.droidboys.goodtrips.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
 
+
+    private TextView mText;
+    private CircleImageView mProf;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -24,7 +30,10 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View mView= inflater.inflate(R.layout.fragment_profile, container, false);
+        mText=(TextView)mView.findViewById(R.id.tv_fp);
+        mProf=(CircleImageView)mView.findViewById(R.id.iv_fp);
+        return mView;
     }
 
 }
